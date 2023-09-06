3a3e034b21d72b85360892bf4163b043ec227109
Nils <nilshoeller00@gmail.com>
2023-09-06 20:57:36 +0200

    event listener - prevents form from submitting

---
+++ script.js
@@ -0,0 +1,9 @@
+const form = document.getElementById("form");
+const distance = document.getElementById("distance");
+const time_from = document.getElementById("time_from");
+const time_to = document.getElementById("time_to");
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+})
+
